74a7e29384192b07e4e8f18b535cf16ab2d1d6c8
Guru Nayak <57138569+I-c-e-m-a-n@users.noreply.github.com>
2023-09-30 23:50:18 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// certifications variables
+const certificationsItem = document.querySelectorAll("[data-certifications-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,30 +28,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const certificationsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < certificationsItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  certificationsItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-certifications-avatar]").src;
+    modalImg.alt = this.querySelector("[data-certifications-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-certifications-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-certifications-text]").innerHTML;
 
-    testimonialsModalFunc();
+    certificationsModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", certificationsModalFunc);
+overlay.addEventListener("click", certificationsModalFunc);
 
 
 
